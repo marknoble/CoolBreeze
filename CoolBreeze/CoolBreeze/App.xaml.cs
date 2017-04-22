1a62f296202e1502ddd470db514aa7aba45abbb0
Mark Noble <mark@marknoble.com>
2017-04-22 01:13:33 -0400

    Add project files.

---
+++ CoolBreeze/CoolBreeze/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CoolBreeze
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel;
+        public static string RegistrationCode = "XBWW4CB95898B1A32B6C1113B510D692";
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CoolBreeze.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
